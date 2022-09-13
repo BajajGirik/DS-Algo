908a54bdd8b734af2464b1bac52455cd4af82f90
Girik Bajaj <bajajgirik2010@gmail.com>
2022-09-13 19:52:16 +0530

    Start cses intro problem set

---
+++ cses/introProblems/permutations.cpp
@@ -0,0 +1,64 @@
+/******************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                long long
+#define vi                vector<ll>
+#define vvi               vector<vi>
+#define all(v)            v.begin(), v.end()
+#define pii               pair<ll, ll>
+#define pb                push_back
+#define mp                make_pair
+#define mid(l, h)         ((l+h)/2)
+#define loop(i, a, b)     for (auto i = a; i < b; ++i)
+#define looprev(i, a, b)  for (auto i = a; i >= b; --i)
+#define rloop(i, a)       for (auto i : a)
+#define rrloop(i, a)      for (auto &i : a)
+#define mod               1'000'000'007
+#define endl              '\n'
+#define ff                first
+#define ss                second
+/******************************************************/
+
+void file_i_o() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r", stdin);
+	freopen("output.txt","w", stdout);
+#endif
+}
+
+void solve() {
+	int n;
+	cin >> n;
+
+	if(n > 1 && n <= 3) {
+		cout << "NO SOLUTION";
+		return;
+	}
+
+	for(int i = 2; i <= n; i+=2) {
+		cout << i << " ";
+	}
+
+	for(int i = 1; i <= n; i+=2) {
+		cout << i << " ";
+	}
+
+}
+
+int main () {
+	clock_t start = clock();
+	file_i_o();
+	int T=1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+	#ifndef ONLINE_JUDGE
+		clock_t end = clock();
+		cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+		<< " sec";
+	#endif
+	return 0;
+}
