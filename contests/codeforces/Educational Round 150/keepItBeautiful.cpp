ec7eb1607a96ca3ac6418f0ec01d85e7ba4ff6a3
Girik Bajaj <bajajgirik2010@gmail.com>
2023-08-02 18:52:19 +0530

    [Codeforces] Start educational round 150

---
+++ contests/codeforces/Educational Round 150/keepItBeautiful.cpp
@@ -0,0 +1,87 @@
+/******************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define all(v) v.begin(), v.end()
+#define pii pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+#define mid(l, h) ((l + h) / 2)
+#define loop(i, a, b) for (auto i = a; i < b; ++i)
+#define looprev(i, a, b) for (auto i = a; i >= b; --i)
+#define rloop(i, a) for (auto i : a)
+#define rrloop(i, a) for (auto &i : a)
+#define mod 1'000'000'007
+#define endl '\n'
+#define ff first
+#define ss second
+/******************************************************/
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    // code goes here
+    int q;
+    cin >> q;
+    int start, prev;
+    bool isSwitched = false;
+
+    cin >> start;
+    prev = start;
+    cout << 1;
+
+    for (int i = 1; i < q; ++i) {
+        int x;
+        cin >> x;
+
+        if (isSwitched) {
+            if (start >= x and x >= prev) {
+                cout << 1;
+            } else {
+                cout << 0;
+                continue;
+            }
+        } else {
+            if (x < prev && start < x) {
+                cout << 0;
+                continue;
+            }
+
+            if (x < prev) {
+                isSwitched = true;
+            }
+
+            cout << 1;
+        }
+        prev = x;
+    }
+
+    cout << "\n";
+}
+
+int main() {
+    clock_t start = clock();
+    file_i_o();
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted in: " << (double)(end - start) / double(CLOCKS_PER_SEC)
+        << " sec";
+#endif
+    return 0;
+}
