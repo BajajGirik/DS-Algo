11b92fd43de2f543d8c12365b781ee073873f8e3
BajajGirik <bajajgirik2010@gmail.com>
2024-09-07 20:12:51 +0530

    [Leetcode] Solve problem 1

---
+++ leetcode/1. Two Sum.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> set = new HashMap<>();
+        int[] result = new int[2];
+
+        for (int i=0; i<nums.length; ++i) {
+          if (set.containsKey(target - nums[i])) {
+            result[0] = set.get(target - nums[i]);
+            result[1] = i;
+            break;
+          }
+          set.put(nums[i], i);
+        }
+
+        return result;
+    }
+ }
